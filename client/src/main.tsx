85b04826668606947f45a1b52dd8f974cc03d13b
llOrtegall <IvanOrtega_97@hotmail.com>
2024-08-22 14:29:50 -0500

    continuar desarrollo

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { AuthProvider } from './auth/AuthProvider.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import router from './routes/index.tsx'
-import { StrictMode } from 'react'
+
 import './index.css'
 import axios from 'axios'
 
@@ -10,9 +10,7 @@ axios.defaults.withCredentials = true
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
-  <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </StrictMode>
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 )
